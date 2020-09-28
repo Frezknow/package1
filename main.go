799b6c436ab4ad0d1f9d926772da080285de1311
Nathan Reynolds <frezknow@gmail.com>
2020-09-28 11:02:39 -0400

    Update main.go

--- main.go
+++ main.go
@@ -1,6 +1,6 @@
 package package1
 //Function sum provides an interface for as many as supplied ints to be summed/added together.
-func Sum(x1 ...int) int{
+func Sum(xi ...int) int{
  sum := 0
  for _, v := range xi{
   sum += v
