d089ce7f086baaa417bb2f9afb32a6736cd73866
nathreyn <nathreyn@cisco.com>
2020-09-28 10:55:19 -0400

    Create main.go

---
+++ main.go
@@ -0,0 +1,9 @@
+package package1
+//Function sum provides an interface for as many as supplied ints to be summed/added together.
+func Sum(x1 ...int) int{
+ sum := 0
+ for _, v := range xi{
+  sum += v
+ }
+
+}
